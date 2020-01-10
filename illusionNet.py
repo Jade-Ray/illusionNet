29ade1bb2f13b71448921fb529cd39be26a5c963
雷琪 <l503976309@gmail.com>
2020-01-10 22:19:43 +0800

    init

---
+++ illusionNet.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+
+plt.ion()
+
+
